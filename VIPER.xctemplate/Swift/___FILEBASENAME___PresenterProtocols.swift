dff0826957344de274a13be7eaae4c442b5d6261
Ryan 'Cold' Bush <ryan.bush@davita.com>
2015-05-12 17:57:05 -0600

    Seperated Swift protocols from main class files

---
+++ VIPER.xctemplate/Swift/___FILEBASENAME___PresenterProtocols.swift
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+// VIPER Interface for communication from Interactor -> Presenter
+protocol ___FILEBASENAMEASIDENTIFIER___InteractorOutput {
+        
+}
+
+// VIPER Interface for communication from View -> Presenter
+protocol ___FILEBASENAMEASIDENTIFIER___PresenterInterface {
+        
+}
+
+// VIPER Interface for communication from Wireframe -> Presenter
+protocol ___FILEBASENAMEASIDENTIFIER___Routing {
+        
+}
---
+++ VIPER.xctemplate/Swift/___FILEBASENAME___WireframeProtocols.swift
@@ -0,0 +1,17 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+// VIPER Interface to the Module
+protocol ___FILEBASENAMEASIDENTIFIER___Delegate {
+        
+}
+
+// VIPER Interface for communication from Presenter -> Wireframe
+protocol ___FILEBASENAMEASIDENTIFIER___WireframeInterface {
+        
+}
