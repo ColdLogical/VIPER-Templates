367579018bd5212e479c48d0b848076e696631eb
Ryan Bush <cold@coldlogical.com>
2023-01-16 19:12:05 +0900

    CHANGE: Reverted to 4 spaces. I waged the battles, but lost the war CHANGE: Updated Icons

--- VIPER Tests.xctemplate/Quick & NimbleSwift 5/___FILEBASENAME___InteractorTests.swift
+++ VIPER Tests.xctemplate/Quick & NimbleSwift 5/___FILEBASENAME___InteractorTests.swift
@@ -3,39 +3,39 @@ import Quick
 
 @testable import ___PROJECTNAME___
 
- // swiftlint:disable:next type_body_length
+// swiftlint:disable:next type_body_length
 class ___VARIABLE_productName___InteractorSpec: QuickSpec {
-        // swiftlint:disable:next function_body_length
-        override func spec() {
-                var interactor: ___VARIABLE_productName___Interactor!
-
-                var presenterMock: ___VARIABLE_productName___InteractorToPresenterInterfaceMock!
-
-                describe("a ___VARIABLE_productName___ interactor") {
-                        beforeEach {
-                                interactor = ___VARIABLE_productName___Interactor()
-                                presenterMock = ___VARIABLE_productName___InteractorToPresenterInterfaceMock()
-
-                                interactor.presenter = presenterMock
-                        }
-
-                        // MARK: - Operational
-
-                        // MARK: - Presenter to Interactor Interface
-                }
+    // swiftlint:disable:next function_body_length
+    override func spec() {
+        var interactor: ___VARIABLE_productName___Interactor!
+        
+        var presenterMock: ___VARIABLE_productName___InteractorToPresenterInterfaceMock!
+        
+        describe("a ___VARIABLE_productName___ interactor") {
+            beforeEach {
+                interactor = ___VARIABLE_productName___Interactor()
+                presenterMock = ___VARIABLE_productName___InteractorToPresenterInterfaceMock()
+                
+                interactor.presenter = presenterMock
+            }
+            
+            // MARK: - Operational
+            
+            // MARK: - Presenter to Interactor Interface
         }
+    }
 }
 
 // MARK: - Communicaiton Interface Mocks
 // MARK: - Interactor to Presenter
 class ___VARIABLE_productName___InteractorToPresenterInterfaceMock {
-        var functionsCalled = [String]()
-
-        // MARK: - Input Variables
+    var functionsCalled = [String]()
+    
+    // MARK: - Input Variables
 }
 
 extension ___VARIABLE_productName___InteractorToPresenterInterfaceMock: ___VARIABLE_productName___InteractorToPresenterInterface {
-
+    
 }
 
 // swiftlint:disable:this file_length
--- VIPER Tests.xctemplate/Quick & NimbleSwift 5/___FILEBASENAME___ViewTests.swift
+++ VIPER Tests.xctemplate/Quick & NimbleSwift 5/___FILEBASENAME___ViewTests.swift
@@ -3,52 +3,52 @@ import Quick
 
 @testable import ___PROJECTNAME___
 
- // swiftlint:disable:next type_body_length
+// swiftlint:disable:next type_body_length
 class ___VARIABLE_productName___ViewSpec: QuickSpec {
-        // swiftlint:disable:next function_body_length
-        override func spec() {
-                var view: ___VARIABLE_productName___View!
-
-                var presenterMock: ___VARIABLE_productName___ViewToPresenterInterfaceMock!
-
-                describe("a ___VARIABLE_productName___ view") {
-                        beforeEach {
-                                view = ___VARIABLE_productName___View()
-                                presenterMock = ___VARIABLE_productName___ViewToPresenterInterfaceMock()
-
-                                _ = view.view
-
-                                view.presenter = presenterMock
-                        }
-
-                        // MARK: - Operational
-
-                        // MARK: - Presenter to View Interface
-                }
+    // swiftlint:disable:next function_body_length
+    override func spec() {
+        var view: ___VARIABLE_productName___View!
+        
+        var presenterMock: ___VARIABLE_productName___ViewToPresenterInterfaceMock!
+        
+        describe("a ___VARIABLE_productName___ view") {
+            beforeEach { @MainActor in
+                view = ___VARIABLE_productName___View()
+                presenterMock = ___VARIABLE_productName___ViewToPresenterInterfaceMock()
+                
+                _ = view.view
+                
+                view.presenter = presenterMock
+            }
+            
+            // MARK: - Operational
+            
+            // MARK: - Presenter to View Interface
         }
+    }
 }
 
 // MARK: - Communicaiton Interface Mocks
 // MARK: - Navigation
 class ___VARIABLE_productName___NavigationInterfaceMock {
-        var functionsCalled = [String]()
-
-        // MARK: - Input Variables
+    var functionsCalled = [String]()
+    
+    // MARK: - Input Variables
 }
 
 extension ___VARIABLE_productName___NavigationInterfaceMock: ___VARIABLE_productName___NavigationInterface {
-
+    
 }
 
 // MARK: - View to Presenter
 class ___VARIABLE_productName___ViewToPresenterInterfaceMock {
-        var functionsCalled = [String]()
-
-        // MARK: - Input Variables
+    var functionsCalled = [String]()
+    
+    // MARK: - Input Variables
 }
 
 extension ___VARIABLE_productName___ViewToPresenterInterfaceMock: ___VARIABLE_productName___ViewToPresenterInterface {
-
+    
 }
 
 // swiftlint:disable:this file_length
