aafcf3d35c44e66c9e89d24c957d0a2d1f729156
Ryan Bush <cold@coldlogical.com>
2023-01-16 19:14:01 +0900

    DELETE: Removed Swift 4 support CHANGE: Reverted to 4 spaces for main files

--- VIPER.xctemplate/Swift 5/___FILEBASENAME___View.swift
+++ VIPER.xctemplate/Swift 5/___FILEBASENAME___View.swift
@@ -1,15 +1,15 @@
 import UIKit
 
 class ___VARIABLE_productName___View: UIViewController {
-        // MARK: - VIPER Stack
-        weak var presenter: ___VARIABLE_productName___ViewToPresenterInterface!
-
-        // MARK: - Instance Variables
-
-        // MARK: - Outlets
-
-        // MARK: - Operational
-
+    // MARK: - VIPER Stack
+    weak var presenter: ___VARIABLE_productName___ViewToPresenterInterface!
+    
+    // MARK: - Instance Variables
+    
+    // MARK: - Outlets
+    
+    // MARK: - Operational
+    
 }
 
 // MARK: - Navigation Interface
@@ -17,11 +17,11 @@ extension ___VARIABLE_productName___View: ___VARIABLE_productName___NavigationIn
 
 // MARK: - Presenter to View Interface
 extension ___VARIABLE_productName___View: ___VARIABLE_productName___PresenterToViewInterface {
-
+    
 }
 
 // MARK: - Communication Interfaces
 // VIPER Interface for communication from View -> Presenter
 protocol ___VARIABLE_productName___ViewToPresenterInterface: AnyObject {
-
+    
 }
--- VIPER.xctemplate/Swift 5/___FILEBASENAME___Wireframe.swift
+++ VIPER.xctemplate/Swift 5/___FILEBASENAME___Wireframe.swift
@@ -1,97 +1,97 @@
 import UIKit
 
 class ___VARIABLE_productName___Wireframe {
-        // MARK: - VIPER Stack
-        lazy var moduleInteractor = ___VARIABLE_productName___Interactor()
-        // Uncomment to use a navigationController from storyboard
-        /*
-        lazy var moduleNavigationController: UINavigationController = {
-                let sb = ___VARIABLE_productName___Wireframe.storyboard()
-                let nc = (sb.instantiateViewController(withIdentifier: ___VARIABLE_productName___Constants.navigationControllerIdentifier) as? UINavigationController)!
-                return nc
-        }()
-        */
-        lazy var modulePresenter = ___VARIABLE_productName___Presenter()
-        lazy var moduleView: ___VARIABLE_productName___View = {
-                // Uncomment the commented line below and delete the storyboard
-                //      instantiation to use a navigationController from storyboard
-                //let vc = self.moduleNavigationController.viewControllers[0] as! ___VARIABLE_productName___View
-                let sb = ___VARIABLE_productName___Wireframe.storyboard()
-                let vc = (sb.instantiateViewController(withIdentifier: ___VARIABLE_productName___Constants.viewIdentifier) as? ___VARIABLE_productName___View)!
-                _ = vc.view
-                return vc
-        }()
-
-        // MARK: - Computed VIPER Variables
-        lazy var presenter: ___VARIABLE_productName___WireframeToPresenterInterface = self.modulePresenter
-        lazy var view: ___VARIABLE_productName___NavigationInterface = self.moduleView
-
-        // MARK: - Instance Variables
-
-        // MARK: - Initialization
-        init() {
-                let i = moduleInteractor
-                let p = modulePresenter
-                let v = moduleView
-
-                i.presenter = p
-
-                p.interactor = i
-                p.view = v
-                p.wireframe = self
-
-                v.presenter = p
-        }
-
-    	class func storyboard() -> UIStoryboard {
-                return UIStoryboard(
-                    name: ___VARIABLE_productName___Constants.storyboardIdentifier,
-                    bundle: Bundle(for: ___VARIABLE_productName___Wireframe.self)
-                )
-    	}
-
-        // MARK: - Operational
-
+    // MARK: - VIPER Stack
+    lazy var moduleInteractor = ___VARIABLE_productName___Interactor()
+    // Uncomment to use a navigationController from storyboard
+    /*
+     lazy var moduleNavigationController: UINavigationController = {
+     let sb = ___VARIABLE_productName___Wireframe.storyboard()
+     let nc = (sb.instantiateViewController(withIdentifier: ___VARIABLE_productName___Constants.navigationControllerIdentifier) as? UINavigationController)!
+     return nc
+     }()
+     */
+    lazy var modulePresenter = ___VARIABLE_productName___Presenter()
+    lazy var moduleView: ___VARIABLE_productName___View = {
+        // Uncomment the commented line below and delete the storyboard
+        //      instantiation to use a navigationController from storyboard
+        //let vc = self.moduleNavigationController.viewControllers[0] as! ___VARIABLE_productName___View
+        let sb = ___VARIABLE_productName___Wireframe.storyboard()
+        let vc = (sb.instantiateViewController(withIdentifier: ___VARIABLE_productName___Constants.viewIdentifier) as? ___VARIABLE_productName___View)!
+        _ = vc.view
+        return vc
+    }()
+    
+    // MARK: - Computed VIPER Variables
+    lazy var presenter: ___VARIABLE_productName___WireframeToPresenterInterface = self.modulePresenter
+    lazy var view: ___VARIABLE_productName___NavigationInterface = self.moduleView
+    
+    // MARK: - Instance Variables
+    
+    // MARK: - Initialization
+    init() {
+        let i = moduleInteractor
+        let p = modulePresenter
+        let v = moduleView
+        
+        i.presenter = p
+        
+        p.interactor = i
+        p.view = v
+        p.wireframe = self
+        
+        v.presenter = p
+    }
+    
+    class func storyboard() -> UIStoryboard {
+        return UIStoryboard(
+            name: ___VARIABLE_productName___Constants.storyboardIdentifier,
+            bundle: Bundle(for: ___VARIABLE_productName___Wireframe.self)
+        )
+    }
+    
+    // MARK: - Operational
+    
 }
 
 // MARK: - Module Interface
 extension ___VARIABLE_productName___Wireframe: ___VARIABLE_productName___ {
-        var delegate: ___VARIABLE_productName___Delegate? {
-                get {
-                        return presenter.delegate
-                }
-                set {
-                        presenter.set(delegate: newValue)
-                }
+    var delegate: ___VARIABLE_productName___Delegate? {
+        get {
+            return presenter.delegate
         }
+        set {
+            presenter.set(delegate: newValue)
+        }
+    }
 }
 
 // MARK: - Presenter to Wireframe Interface
 extension ___VARIABLE_productName___Wireframe: ___VARIABLE_productName___PresenterToWireframeInterface {
-
+    
 }
 
 // MARK: - Communication Interfaces
 // Interface Abstraction for working with the VIPER Module
 protocol ___VARIABLE_productName___: AnyObject {
-        var delegate: ___VARIABLE_productName___Delegate? { get set }
+    var delegate: ___VARIABLE_productName___Delegate? { get set }
 }
 
 // VIPER Module Constants
 struct ___VARIABLE_productName___Constants {
-        // Uncomment to utilize a navigation controller from storyboard
-        //static let navigationControllerIdentifier = "___VARIABLE_productName___NavigationController"
-        static let storyboardIdentifier = "___VARIABLE_productName___"
-        static let viewIdentifier = "___VARIABLE_productName___View"
+    // Uncomment to utilize a navigation controller from storyboard
+    //static let navigationControllerIdentifier = "___VARIABLE_productName___NavigationController"
+    static let storyboardIdentifier = "___VARIABLE_productName___"
+    static let viewIdentifier = "___VARIABLE_productName___View"
 }
 
 // VIPER Interface for manipulating the navigation of the view
 protocol ___VARIABLE_productName___NavigationInterface: AnyObject {
-
+    
 }
 
 // VIPER Interface for communication from Wireframe -> Presenter
 protocol ___VARIABLE_productName___WireframeToPresenterInterface: AnyObject {
-        var delegate: ___VARIABLE_productName___Delegate? { get }
-        func set(delegate newDelegate: ___VARIABLE_productName___Delegate?)
+    var delegate: ___VARIABLE_productName___Delegate? { get }
+    func set(delegate newDelegate: ___VARIABLE_productName___Delegate?)
 }
